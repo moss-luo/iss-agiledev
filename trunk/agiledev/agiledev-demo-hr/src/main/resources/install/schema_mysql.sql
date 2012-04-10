74100400823b300f1f52b5e1313817ddfed4c5a1
putao4914968@gmail.com <putao4914968@gmail.com@9cf4da03-0f77-f790-1614-1b1d250d2612>
2012-04-10 15:17:09 +0000

    添加了一个树的demo！

--- trunk/agiledev/agiledev-demo-hr/src/main/resources/install/schema_mysql.sql
+++ trunk/agiledev/agiledev-demo-hr/src/main/resources/install/schema_mysql.sql
@@ -19,14 +19,14 @@ CREATE TABLE  `agiledev`.`T_LEVEL` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `agiledev`.`T_PERMISION` (
-  `uid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
   `permisionName` char(200) NOT NULL,
   `url` char(255),
   `pid` int(11) NOT NULL,
   `pname` int(11),
   `hasChild` int(11),
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `agiledev`.`T_ROLE` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
@@ -52,22 +52,22 @@ CREATE TABLE  `agiledev`.`T_USER` (
 
 
 
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(0,'root','','-1',1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(1,'system','',0,1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(2,'basic','',0,1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(3,'chart','',0,1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(4,'report','',0,1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(5,'data','',0,1);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(6,'users','system/user.html',1,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(7,'roles','system/role.html',1,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(8,'modules','system/module.html',1,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(9,'departments','basedata/department.html',2,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(10,'employee','basedata/employee.html',2,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(11,'train type','basedata/train.html',2,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(12,'train','basedata/programs.html',2,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(13,'level','basedata/level.html',2,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(14,'department train chart','',3,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(15,'level train chart','',3,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(16,'department train report','',4,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(17,'level train report','',4,0);
-insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(18,'train data','',5,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(1,'root','','-1',1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(2,'system','',1,1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(3,'basic','',1,1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(4,'chart','',1,1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(5,'report','',1,1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(6,'data','',1,1);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(7,'users','system/user.html',2,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(8,'roles','system/role.html',2,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(9,'modules','system/module.html',2,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(10,'departments','basedata/department.html',3,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(11,'employee','basedata/employee.html',3,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(12,'train type','basedata/train.html',3,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(13,'train','basedata/programs.html',3,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(14,'level','basedata/level.html',3,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(15,'department train chart','',4,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(16,'level train chart','',4,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(17,'department train report','',5,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(18,'level train report','',5,0);
+insert into T_PERMISION (uid,permisionname,url,pid,hasChild) values(19,'train data','',6,0);
