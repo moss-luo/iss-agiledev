63db9d6069d27a20e5bde05ca84d161beca647f0
putao4914968 <putao4914968@gmail.com>
2012-05-10 17:05:57 +0000

    添加了ligerui的list demo页面

---
+++ agiledev/agiledev-demo-hr/src/main/java/com/isoftstone/agiledev/actions/system/user/LigeruiListAction.java
@@ -0,0 +1,47 @@
+package com.isoftstone.agiledev.actions.system.user;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.struts2.convention.annotation.Result;
+
+import com.isoftstone.agiledev.manages.BaseService;
+import com.isoftstone.agiledev.pager.PageResult;
+import com.isoftstone.agiledev.pager.PaginationInterface;
+import com.isoftstone.agiledev.query.QueryParametersMap;
+import com.isoftstone.agiledev.query.SummaryProvider;
+
+@Result(name="result",type="ligerui-datagrid-json",params={"root","results"})
+public class LigeruiListAction implements SummaryProvider{
+
+	private List<User> results = null;
+	@Resource(name="baseService")
+	private BaseService<User> userService = null;
+	@QueryParametersMap
+	private Map<String,Object> queryCondition = new HashMap<String,Object>();
+	
+	public String execute(){
+		results = userService.list(queryCondition, new User());
+		return "result";
+	}
+
+	public Map<String, Object> getQueryCondition() {
+		return queryCondition;
+	}
+
+	public void setQueryCondition(Map<String, Object> queryCondition) {
+		this.queryCondition = queryCondition;
+	}
+
+	public List<User> getResults() {
+		return results;
+	}
+
+	@Override
+	public int getTotal() {
+		return userService.getTotal();
+	}
+}
