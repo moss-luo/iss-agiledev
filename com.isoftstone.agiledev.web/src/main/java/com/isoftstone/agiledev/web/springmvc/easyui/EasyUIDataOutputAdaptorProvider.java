8d3f010bc7b229755bdd285d8f1f4ad8b8b3900d
putao4914968 <putao4914968@gmail.com>
2012-11-01 08:56:18 +0000

    暂时补丁：解决ligerUI适配后EasyUIValidateInitial的bundleContext为null的问题。 后期需要修改到com.isoftstone.agiledev.config读取配置。web模块下本身于osgi无关。

--- com.isoftstone.agiledev.web/src/main/java/com/isoftstone/agiledev/web/springmvc/easyui/EasyUIDataOutputAdaptorProvider.java
+++ com.isoftstone.agiledev.web/src/main/java/com/isoftstone/agiledev/web/springmvc/easyui/EasyUIDataOutputAdaptorProvider.java
@@ -3,17 +3,28 @@ package com.isoftstone.agiledev.web.springmvc.easyui;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.osgi.framework.BundleContext;
+
 import com.isoftstone.agiledev.web.springmvc.DataOutputAdaptor;
 import com.isoftstone.agiledev.web.springmvc.DataOutputAdaptorProvider;
 
 public class EasyUIDataOutputAdaptorProvider implements DataOutputAdaptorProvider{
 
 	private Map<String,DataOutputAdaptor> adaptors = new HashMap<String,DataOutputAdaptor>();
+
+	private BundleContext bundleContext = null;
+	public EasyUIDataOutputAdaptorProvider() {
+	}
+	public EasyUIDataOutputAdaptorProvider(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+	
 	@Override
 	public void init() {
 		DataOutputAdaptor adaptor = new EasyUIGridDataOutputAdaptor();
 		adaptors.put(adaptor.getType(), adaptor);
-		adaptor = new EasyUIInitOutputAdaptor();
+		adaptor = new EasyUIInitOutputAdaptor(bundleContext);
 		adaptors.put(adaptor.getType(), adaptor);
 		adaptor = new EasyUITreeDataOutputAdaptor();
 		adaptors.put(adaptor.getType(), adaptor);
--- com.isoftstone.agiledev.web/src/main/java/com/isoftstone/agiledev/web/springmvc/easyui/EasyUIView.java
+++ com.isoftstone.agiledev.web/src/main/java/com/isoftstone/agiledev/web/springmvc/easyui/EasyUIView.java
@@ -1,14 +1,23 @@
 package com.isoftstone.agiledev.web.springmvc.easyui;
 
+import org.eclipse.gemini.blueprint.context.BundleContextAware;
+import org.osgi.framework.BundleContext;
+
 import com.isoftstone.agiledev.web.springmvc.DataOutputAdaptorProvider;
 import com.isoftstone.agiledev.web.springmvc.DefaultView;
 
-public class EasyUIView extends DefaultView {
+public class EasyUIView extends DefaultView implements BundleContextAware{
+	private BundleContext bundleContext = null;
 	@Override
 	public DataOutputAdaptorProvider getDataOutputAdaptorProvider() {
-		EasyUIDataOutputAdaptorProvider  provider = new EasyUIDataOutputAdaptorProvider();
+		EasyUIDataOutputAdaptorProvider  provider = new EasyUIDataOutputAdaptorProvider(bundleContext);
 		provider.init();
 		return provider;
 
+	}
+
+	@Override
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
 	}
 }
