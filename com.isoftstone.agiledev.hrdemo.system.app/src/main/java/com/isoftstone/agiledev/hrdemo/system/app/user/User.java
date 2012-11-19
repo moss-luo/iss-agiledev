ecdc3492a40ac1ebfcef2ec31a8bd263f611c66d
jimmyjin1898 <jimmyjin1898@gmail.com>
2012-11-19 08:05:58 +0000

    多重验证及版本升级

--- com.isoftstone.agiledev.hrdemo.system.app/src/main/java/com/isoftstone/agiledev/hrdemo/system/app/user/User.java
+++ com.isoftstone.agiledev.hrdemo.system.app/src/main/java/com/isoftstone/agiledev/hrdemo/system/app/user/User.java
@@ -1,5 +1,7 @@
 package com.isoftstone.agiledev.hrdemo.system.app.user;
 
+
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Required;
@@ -7,47 +9,49 @@ import org.springframework.beans.factory.annotation.Required;
 import com.isoftstone.agiledev.core.init.Init;
 
 public class User {
-	
+
 	private Integer id;
-	@Init(fieldName="name",value="zhangsan")
-	@Length(min=1,max=10,message="the length must be in 1-10.")
+	@Init(fieldName = "name", value = "zhangsan")
+	@Length(min = 1, max = 10, message = "the length must be in 1-10.")
 	private String name;
-	@Init(fieldName="password",value="123123")
+	@Init(fieldName = "password", value = "123123")
 	private String password;
-	@NotEmpty(message="mobile not null")
-	private String  mobile;
 	
-	@org.hibernate.validator.constraints.Email(message="the invalid email")
-	private String  email;
-	
-	
-	public User(Integer id, String name, String password,String mobile,String email) {
+	@NotEmpty(message = "mobile not null")
+	private String mobile;
+
+	@Length(min = 5, max = 10, message = "the length must be in 5-10.")
+	@Email(message = "the invalid email")
+	private String email;
+
+	public User(Integer id, String name, String password, String mobile,
+			String email) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 		this.mobile = mobile;
 		this.email = email;
 	}
-	
+
 	public User() {
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
@@ -72,5 +76,6 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-}
 
+
+}
